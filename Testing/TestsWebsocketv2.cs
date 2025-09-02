eac4b2a86558af91cc4dfd67f5f1c7398f7c4de5
tesni <tesnileft@gmail.com>
2025-09-02 14:13:27 +0200

    Add Testing project

---
+++ Testing/TestsWebsocketv2.cs
@@ -0,0 +1,15 @@
+namespace TestProject1;
+
+public class WebsocketTestsv2
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
